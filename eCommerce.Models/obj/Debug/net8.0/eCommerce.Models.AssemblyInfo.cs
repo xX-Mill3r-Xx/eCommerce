f6439f208eb6430e951c5548991b3e7e7ec5bd24
Unknown <millermartinsvasco1@gmail.com>
2025-07-04 21:05:02 -0300

    ✨ feat: criando a model de usuario

--- eCommerce.Models/obj/Debug/net8.0/eCommerce.Models.AssemblyInfo.cs
+++ eCommerce.Models/obj/Debug/net8.0/eCommerce.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a97c08d6d7541c78dad3e353246b58d3128ff79")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
